3febebf5730dc79ac39bd0b59cdccb795fff4c96
Linyi Guo <vickyguo@Linyis-MacBook-Pro.local>
2020-10-28 15:01:00 -0700

    php added

--- guo.linyi/README.md
+++ guo.linyi/README.md
@@ -7,4 +7,4 @@
 - http://guolinyi.com/aau/wnm608/guo.linyi/recipes/index.html
 - http://guolinyi.com/aau/wnm608/guo.linyi/styleguide/
 - http://guolinyi.com/aau/wnm608/guo.linyi/styleguide/index.html
-
+- http://guolinyi.com/aau/wnm608/guo.linyi/demo/learning_php.php
---
+++ guo.linyi/parts/navbar.php
@@ -0,0 +1,15 @@
+<header class="navbar">
+   <div class="container display-flex flex-align-center">
+      <div class="flex-none"><h1><a href="index.php">Store Website</a></h1></div>
+      <div class="flex-stretch"></div>
+      
+      <!-- nav.nav>ul>li*4>a[href=#article$]>{Link $} -->
+      <nav class="nav flex-none">
+         <ul class="display-flex">
+            <li><a href="product_list.php">Shop</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="product_cart.php">Cart</a></li>
+         </ul>
+      </nav>
+   </div>
+</header>
